1fe364c34348ca17aab52695660c065886613a71
Jeffrey Moore <jmoore987@yahoo.com>
2016-01-04 18:10:22 -0800

    Release v0.2

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 setup(
     name = 'sharp_aquos_rc',
     packages = ['sharp_aquos_rc'],
-    version = '0.1',
+    version = '0.2',
     description = 'Control Sharp Aquos SmartTVs through the IP interface.',
     author = 'Jeffrey Moore',
     author_email = 'jmoore987@yahoo.com',
     url = 'https://github.com/jmoore987/sharp_aquos_rc',
-    download_url = 'https://github.com/jmoore987/sharp_aquos_rc/tarball/0.1',
+    download_url = 'https://github.com/jmoore987/sharp_aquos_rc/tarball/0.2',
     keywords = ['sharp', 'aquos', 'remote', 'control', 'tv', 'smarttv', 'iot' ],
     classifiers = []
 )
