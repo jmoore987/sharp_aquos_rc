9a5a1140241a866150dea2d26555680dc8e7f057
jmoore987 <jmoore987@yahoo.com>
2017-02-27 10:52:08 -0800

    Update setup.py
    Prepare for new release

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ setup(
     name = 'sharp_aquos_rc',
     packages = ['sharp_aquos_rc'],
     package_data={'sharp_aquos_rc': ['commands/*.yaml']},
-    version = '0.3.3',
+    version = '0.4',
     description = 'Control Sharp Aquos SmartTVs through the IP interface.',
     author = 'Jeffrey Moore',
     author_email = 'jmoore987@yahoo.com',
     url = 'https://github.com/jmoore987/sharp_aquos_rc',
-    download_url = 'https://github.com/jmoore987/sharp_aquos_rc/tarball/0.3.3',
+    download_url = 'https://github.com/jmoore987/sharp_aquos_rc/tarball/0.4',
     keywords = ['sharp', 'aquos', 'remote', 'control', 'tv', 'smarttv', 'iot' ],
     classifiers = []
 )
